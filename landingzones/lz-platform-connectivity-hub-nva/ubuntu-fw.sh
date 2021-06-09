639a426b95cc5a1f4aa0b815fc317e7c2cc924ed
Senthuran Sivananthan <sesivan@microsoft.com>
2021-06-09 06:52:17 -0400

    Re-IP hub networking

--- landingzones/lz-platform-connectivity-hub-nva/ubuntu-fw.sh
+++ landingzones/lz-platform-connectivity-hub-nva/ubuntu-fw.sh
@@ -10,23 +10,23 @@ sudo sysctl -p
 #and add 201 mrz and 202 int
 echo "201 mrz" | sudo tee -a /etc/iproute2/rt_tables
 echo "202 int" | sudo tee -a /etc/iproute2/rt_tables
-sudo ip rule add from 10.22.0.37 to 168.63.129.16 lookup mrz
-sudo ip rule add from 10.22.0.101 to 168.63.129.16 lookup int
+sudo ip rule add from 10.18.0.37 to 168.63.129.16 lookup mrz
+sudo ip rule add from 10.18.0.101 to 168.63.129.16 lookup int
 
 #WARNING this is not permanent!! also watch out for the ETHX number
-sudo ip route add 168.63.129.16 via 10.22.0.33 dev eth2 table mrz
-sudo ip route add 168.63.129.16 via 10.22.0.97 dev eth1 table int
+sudo ip route add 168.63.129.16 via 10.18.0.33 dev eth2 table mrz
+sudo ip route add 168.63.129.16 via 10.18.0.97 dev eth1 table int
 
 #the routes below aren't enough, as it duplicates a route but the default GW takes precedence
 #that's why you need multiple rt_tables
-sudo route add 168.63.129.16 gw 10.22.0.33 metric 100
-sudo route add 168.63.129.16 gw 10.22.0.97 metric 100
+sudo route add 168.63.129.16 gw 10.18.0.33 metric 100
+sudo route add 168.63.129.16 gw 10.18.0.97 metric 100
 
 #now a general route to MRZ
-route add -net 10.22.4.0/22 gw 10.22.0.97 dev eth1 
+route add -net 10.18.4.0/22 gw 10.18.0.97 dev eth1 
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8080 -j DNAT --to 10.22.4.132:80
-sudo iptables -A FORWARD -p tcp -d 10.22.4.132 --dport 80 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8080 -j DNAT --to 10.18.4.132:80
+sudo iptables -A FORWARD -p tcp -d 10.18.4.132 --dport 80 -j ACCEPT
 
 sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
 sudo iptables-save | sudo tee -a /etc/iptables/rules.v4
