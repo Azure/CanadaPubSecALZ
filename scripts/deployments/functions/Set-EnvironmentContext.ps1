5802687af14a09227caff7e257e528fea4ebe89b
Senthuran Sivananthan <sesivan@microsoft.com>
2022-04-28 17:02:33 -0400

    Ensure parameters are mandatory

--- scripts/deployments/functions/Set-EnvironmentContext.ps1
+++ scripts/deployments/functions/Set-EnvironmentContext.ps1
@@ -4,7 +4,10 @@ Import-Module powershell-yaml
 
 function Set-EnvironmentContext {
   param (
+    [Parameter(Mandatory = $true)]
     [string] $WorkingDirectory,
+
+    [Parameter(Mandatory = $true)]
     [string] $Environment
   )
 
@@ -14,7 +17,12 @@ function Set-EnvironmentContext {
   $global:NetworkingDirectory = "$WorkingDirectory/config/networking/$Environment"
 
  
+  # Load main environment variables file as YAML
   $global:EnvironmentConfiguration = Get-Content $EnvironmentConfigurationYamlFilePath  | ConvertFrom-Yaml
+
+  # Retrieve the management group hierarchy variable as JSON
   $global:ManagementGroupHierarchy = $EnvironmentConfiguration.variables['var-managementgroup-hierarchy'] | ConvertFrom-Json
+
+  # Identify the top level management group (the first child underneath Tenant Root Group)
   $global:TopLevelManagementGroupId = $ManagementGroupHierarchy.children[0].id
 }
