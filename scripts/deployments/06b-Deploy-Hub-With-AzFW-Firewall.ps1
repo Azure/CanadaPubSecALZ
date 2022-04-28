cc8a3ea0dda97cdbb3f587fbc0fac16007a3f654
Senthuran Sivananthan <sesivan@microsoft.com>
2022-04-28 12:04:50 -0400

    Scaffolding for Hub + AzFW

---
+++ scripts/deployments/06b-Deploy-Hub-With-AzFW-Firewall.ps1
@@ -0,0 +1,36 @@
+Import-Module powershell-yaml
+
+# Configuration
+$WorkingDirectory = "../../"
+$Environment = "CanadaESLZ-main"
+
+$LoggingDirectory = "$WorkingDirectory/config/logging/$Environment/"
+$NetworkingDirectory = "$WorkingDirectory/config/networking/$Environment/"
+
+$EnvironmentConfigurationYamlFilePath = "$WorkingDirectory/config/variables/$Environment.yml"
+
+$EnvironmentConfiguration = Get-Content $EnvironmentConfigurationYamlFilePath  | ConvertFrom-Yaml
+
+$LoggingSubscription = $EnvironmentConfiguration.variables['var-logging-subscriptionId']
+$LoggingConfigurationFileName = $EnvironmentConfiguration.variables['var-logging-configurationFileName']
+$LoggingConfigurationFilePath = "$LoggingDirectory/$LoggingConfigurationFileName"
+
+$DeploymentRegion = $EnvironmentConfiguration.variables['var-hubnetwork-region']
+$DeploymentManagementGroup = $EnvironmentConfiguration.variables['var-hubnetwork-managementGroupId']
+$DeploymentSubscription = $EnvironmentConfiguration.variables['var-hubnetwork-subscriptionId']
+$DeploymentConfigurationFileName = $EnvironmentConfiguration.variables['var-hubnetwork-azfw-configurationFileName']
+$FirewallPolicyConfigurationFileName = $EnvironmentConfiguration.variables['var-hubnetwork-azfwPolicy-configurationFileName']
+
+# Deployment
+
+# TODO: Load logging configuration
+
+# TODO: Load networking configuration and check if Log Analytics Workspace Id is provided.  Otherwise set it.
+
+# TODO: Load networking configuration and check if Firewall Policy is provided.  Otherwise set it.
+
+Write-Output "Moving Subscription ($DeploymentSubscription) to Management Group ($DeploymentManagementGroup)"
+# TODO: Add Azure PS deployment command
+
+Write-Output "Deploying $NetworkingDirectory/$DeploymentConfigurationFileName to $DeploymentSubscription in $DeploymentRegion"
+# TODO: Add Azure PS deployment command
