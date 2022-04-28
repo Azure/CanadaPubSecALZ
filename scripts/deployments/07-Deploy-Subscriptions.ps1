cb051b6f4f7540bd0db2562e3ebcd9873668fbac
Senthuran Sivananthan <sesivan@microsoft.com>
2022-04-28 13:43:33 -0400

    Scaffolding for subscriptions

---
+++ scripts/deployments/07-Deploy-Subscriptions.ps1
@@ -0,0 +1,21 @@
+. ".\helpers\Set-EnvironmentContext.ps1"
+
+# Working Directory
+$WorkingDirectory = "../.."
+
+# Set Context
+Set-EnvironmentContext -Environment "CanadaESLZ-main" -WorkingDirectory $WorkingDirectory
+
+$Subscriptions = $()
+
+foreach ($subscription in $subscriptions) {
+  # TODO: Find the ARM JSON parameters
+
+  # TODO: Ensure there's only 1 parameters file for each subscription
+
+  # TODO: Parse the file name to determine archetype, region and subscription id
+
+  # TODO: Load networking configuration and check if Log Analytics Workspace Id is provided.  Otherwise set it.
+
+  # TODO: Add Azure PS deployment command
+}
